611f039185fc1212ae128c140ba20fba95096437
vs.kozh@list.ru <vs.kozh@list.ru>
2020-05-19 22:47:32 +0300

    подготовка основы для реализации методов класса

---
+++ SortingAlgorithms/BubbleSort.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms
+{
+    /// <summary>
+    /// Класс описывающий алгоритм сортировки пузырьком
+    /// </summary>
+    public static class BubbleSort
+    {
+
+        public static void RunSortMethod()
+        {
+
+        }
+
+        private static void SwapElems(ref int e1, ref int e2)
+        {
+
+        }
+
+        private static int[] ExecuteBubbleSort( int[] array)
+        {
+
+            return array;
+        }
+
+
+    }
+}
