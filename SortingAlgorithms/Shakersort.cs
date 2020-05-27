2cdd7b10facd90a2736090997de1c38bcdd63f8a
vs.kozh@list.ru <vs.kozh@list.ru>
2020-05-27 11:52:59 +0300

    Создана тестовая ветвь для отработки алгоритма шейкерной сортировки

---
+++ SortingAlgorithms/Shakersort.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms
+{
+    /// <summary>
+    /// Класс, описывающий Сортировку Перемешиванием / Шейкерную (коктейльную сортировку) сортировку
+    /// </summary>
+    class Shakersort
+    {
+
+    }
+}
